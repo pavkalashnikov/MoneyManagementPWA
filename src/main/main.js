14370549ac7dabfa84540185cf1f71ac5a0a4b04
p.kalashnikov <p.kalashnikov@itransition.com>
2019-05-12 17:31:47 +0300

    Initial commit

---
+++ src/main/main.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    // fetch("hh.txt?mode=nocache").then(data => data.text()).then(data => {
+    //     animateText(data)
+    // });
+
+    animateText('Hello Marusya');
+}
+
+function animateText(data) {
+    var ele = document.getElementById("text"),
+        txt = data.split("");
+    var interval = setInterval(function () {
+        if (!txt[0]) {
+            return clearInterval(interval);
+        };
+        ele.innerHTML += txt.shift();
+    }, 150);
+}
